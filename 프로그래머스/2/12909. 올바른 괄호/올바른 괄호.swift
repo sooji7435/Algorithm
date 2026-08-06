f22c9523447ca0dcd236a2c076288b15480604c3
sooji7435 <bys0634@naver.com>
2026-08-06 17:41:46 +0900

    [level 2] Title: 올바른 괄호, Time: 13.44 ms, Memory: 20.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.swift
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.swift
@@ -1,38 +1,11 @@
 import Foundation
 
-// 맨 처음이 )괄호 시작하거나 맨 끝이 (괄호로 끝나면 무조건 false가 된다.
-// (괄호의 개수와 )괄호의 개수가 같아야 true. 반례 "())(()"
-
-// 스택으로 (괄호 다음에 )괄호가 오면 제거
-
 func solution(_ s:String) -> Bool
 {
     var ans:Bool = false
     
-    if s.first == ")" || s.last == "(" {
-        return false
-    } else {
-        var stack: [Character] = []
-        
-        for str in s {
-            if stack.isEmpty {
-                stack.append(str)
-            } else {
-                if stack.last == "(" && str == ")" {
-                    stack.removeLast()
-                } else {
-                    stack.append(str)
-                }
-                
-            }
-        }
-        
-        if stack.isEmpty {
-            return true
-        } else { return false }
-        
-    }
-    
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print("Hello Swift")
 
     return ans
 }
